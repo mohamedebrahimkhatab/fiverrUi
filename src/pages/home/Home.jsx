da543f83b43cbee82ca3eecb48416050f3950b39
mohamedebrahimkhatab <mohamedebrahimkhatab@gmail.com>
2023-05-13 04:15:58 +0300

    integrate home's component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+
 import "./Home.scss";
+import { cards } from "../../data";
+import Slide from "../../components/slide/Slide";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import CatCard from "../../components/catCatd/CatCard";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={4}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
+    </div>
+  );
 };
 
 export default Home;
